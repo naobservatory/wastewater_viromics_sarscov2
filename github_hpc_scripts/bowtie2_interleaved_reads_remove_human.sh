bf9d0655c93b1f2c6bfad060f068fa323b5e379a
Jeff Kaufman <jeff@securebio.org>
2022-10-26 14:01:36 +0000

    bowtie2_interleaved_reads_remove_human: convert to our infra, and download pre-req

--- github_hpc_scripts/bowtie2_interleaved_reads_remove_human.sh
+++ github_hpc_scripts/bowtie2_interleaved_reads_remove_human.sh
@@ -1,18 +1,35 @@
 #!/bin/bash
-#--------------------------SBATCH settings------
 
-#SBATCH --job-name=bowtie2_human      ## job name
-#SBATCH -A katrine_lab     ## account to charge
-#SBATCH -p standard          ## partition/queue name
-#SBATCH --nodes=1            ## (-N) number of nodes to use
-#SBATCH --ntasks=1           ## (-n) number of tasks to launch
-#SBATCH --cpus-per-task=60    ## number of cores the job needs
-#SBATCH --error=slurm-%J.err ## error log file
-#SBATCH --output=slurm-%J.out ##output info file
+if [ ! -e grch38_1kgmaj.1.bt2 ]; then
+    if [ ! -e grch38_1kgmaj.zip ] ; then
+        # per https://github.com/BenLangmead/bowtie2/issues/329
+        wget https://genome-idx.s3.amazonaws.com/bt/grch38_1kgmaj.zip
+    fi
+    unzip grch38_1kgmaj.zip
+fi
 
-module load bowtie2
+for f in $(aws s3 ls s3://prjna729801/ | \
+               awk '{print $NF}' | \
+               grep dedup.fastq.gz$ | \
+               sed s/.dedup.fastq.gz//); do
+    in="${f}.dedup.fastq.gz"
+    out_human="${f}_human_reads.sam"
+    out_base="${f}.dedup.nohuman.fastq.gz"
+    out1="${f}.dedup.nohuman.fastq.1.gz"
+    out2="${f}.dedup.nohuman.fastq.2.gz"
 
-for f in $(ls *.dedupe.fastq.gz | sed 's/.dedupe.fastq.gz//' | sort -u)
-do
-bowtie2 -x grch38_1kgmaj -p 60 -S ${f}_human_reads.sam --interleaved ${f}.dedupe.fastq.gz --un-conc-gz ${f}.dedupe.nohuman.fastq.gz
+    aws s3 cp "s3://prjna729801/$in" $in
+
+    ~/bowtie2-2.4.5-linux-x86_64/bowtie2 \
+        -x grch38_1kgmaj -p 60 -S $out_human \
+        --interleaved $in \
+        --un-conc-gz $out_base
+
+    aws s3 cp $out1 s3://prjna729801/$out1
+    aws s3 cp $out2 s3://prjna729801/$out2
+
+    rm $in
+    rm $out1
+    rm $out2
+    rm $out_human
 done
