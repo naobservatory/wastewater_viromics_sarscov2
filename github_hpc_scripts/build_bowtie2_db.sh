a07c8085873d66fdd58d1ae82511a0516845d2ca
Jeff Kaufman <jeff@securebio.org>
2022-10-26 13:28:35 +0000

    build_bowtie2_db: refactor

--- github_hpc_scripts/build_bowtie2_db.sh
+++ github_hpc_scripts/build_bowtie2_db.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-
-if [ ! -e flag.db_downloaded ]; then
+if [ ! -e all_virus_genomes.fna ]; then
     curl https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/ -sS | \
         grep -E -o 'viral[.][0-9.]+[.]genomic.fna.gz' | \
         sort | uniq | \
         xargs -P 32 -I {} wget \
               https://ftp.ncbi.nlm.nih.gov/refseq/release/viral/{}
-    touch flag.db_downloaded
-fi
-
-if [ ! -e flag.gunzipped ]; then
     gunzip *.genomic.fna.gz
-    touch flag.gunzipped
+    cat *.fna > all_virus_genomes.fna
 fi
 
 if [ ! -e all_virus_genomes_bowtie_db ]; then
-    ~/bowtie2-2.4.5-linux-x86_64/bowtie2-build $(ls *.fna | tr '\n' ',') \
+    ~/bowtie2-2.4.5-linux-x86_64/bowtie2-build all_virus_genomes.fna \
          all_virus_genomes_bowtie_db -f --threads 30
-   rm *.fna
 fi
-
-
